18c81cd1f6beebf9c3c31db0e110fe71a7610b93
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-02-27 01:35:23 -0500

    added support for url links

--- src/components/SingleField.tsx
+++ src/components/SingleField.tsx
@@ -5,16 +5,36 @@ import { toTitleCase } from "../utils/utils";
 
 type SingleFieldProps = {
   title: string;
-  value: string;
+  value: string | string[];
 }
 
 // Component //
 
 const SingleField = ({title, value}: SingleFieldProps) => {
+  let content;
+
+  if (Array.isArray(value)) {
+    content = title.toLowerCase().includes('url') ? (
+      value.map((val, index) => (
+        <React.Fragment key={index}>
+          <a href={val} target="_blank" rel="noopener noreferrer">{val}</a>
+          {index < value.length - 1 ? ', ' : ''}
+        </React.Fragment>
+      ))
+    ) : (
+      value.join(', ')
+    );
+  } else {
+    content = title.toLowerCase().includes(('url')) ? (
+      <a href={value} target="_blank" rel="noopener noreferrer">{value}</a>
+    ) : (
+      value
+    );
+  }
   return (
     <div className="field">
       <div className="field-title">{toTitleCase(title)}:</div>
-      <div className="field-value">{value}</div>
+      <div className="field-value">{content}</div>
     </div>
   )
 }
