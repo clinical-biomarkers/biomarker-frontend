bad3c76e37be99e29b5f5fdd7d7e0d9d635d2ba5
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-02-27 01:35:03 -0500

    working on component rendering

--- src/components/BiomarkerTable.tsx
+++ src/components/BiomarkerTable.tsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { BiomarkerModel } from "../types/BiomarkerTypes";
 import config from "../conf.json";
+import BiomarkerComponentTable from "./BiomarkerComponentTable";
+import Collapsible from "./Collapsible";
+import SingleField from "./SingleField";
+import './BiomarkerTable.css';
+import loading from '../artifacts/loading.gif'
 
 // Typescript stuff //
 
@@ -12,14 +17,20 @@ type BiomarkerTableProps = {
 
 const BiomarkerTable = ({ biomarker_id }: BiomarkerTableProps) => {
   const [biomarker, setBiomarker] = useState<BiomarkerModel | null>(null);
+  const [results, setResults] = useState<number>(0);
+  const status: string = "Reviewed" 
 
   useEffect(() => {
     const fetchData = async () => {
       if (biomarker_id) {
         try {
+          setResults(0);
           const response = await fetch(`${config.api_root}${biomarker_id}`);
-          const data: BiomarkerModel = await response.json();
-          setBiomarker(data);
+          if (response.ok) {
+            const data: BiomarkerModel = await response.json();
+            setBiomarker(data);
+            setResults(1);
+          }
         } catch (error) {
           console.error("Error fetching biomarker data: ", error);
         }
@@ -29,16 +40,22 @@ const BiomarkerTable = ({ biomarker_id }: BiomarkerTableProps) => {
   }, [biomarker_id]);
 
   if (!biomarker) {
-    return <div>Loading...</div>;
+    return (
+      <div className="loading-container">
+        <img src={loading} alt="Loading..."/>
+      </div>
+    )
   }
 
   return (
-    <div>
-      <h1>Results for Biomarker ID: {biomarker_id}</h1>
-      <br />
-      <h3>Biomarker ID</h3>
-      <br />
-      <div>{biomarker.biomarker_id}</div>
+    <div className="results-container">
+        <h1 id="top-header">{biomarker.biomarker_id} Results</h1>
+        <div id="header-label">Results: {results}</div>
+      <Collapsible title='Test'>
+        <SingleField title="Biomarker ID" value={biomarker.biomarker_id}/>
+        <SingleField title="Status" value={status}/>
+        <BiomarkerComponentTable biomarker={biomarker} status={status}/>
+      </Collapsible>
     </div>
   );
 };
