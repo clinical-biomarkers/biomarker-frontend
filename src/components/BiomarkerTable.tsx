4cb910a2c8247c15fe3364e9acd86ae28f0c3ed0
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-02-27 12:31:38 -0500

    add loading gif and other top level fields

--- src/components/BiomarkerTable.tsx
+++ src/components/BiomarkerTable.tsx
@@ -4,8 +4,10 @@ import config from "../conf.json";
 import BiomarkerComponentTable from "./BiomarkerComponentTable";
 import Collapsible from "./Collapsible";
 import SingleField from "./SingleField";
-import './BiomarkerTable.css';
-import loading from '../artifacts/loading.gif'
+import { toTitleCase } from "../utils/utils";
+import "./BiomarkerTable.css";
+import loading from "../artifacts/loading.gif";
+import EvidenceSourceTable from "./EvidenceSource";
 
 // Typescript stuff //
 
@@ -18,7 +20,7 @@ type BiomarkerTableProps = {
 const BiomarkerTable = ({ biomarker_id }: BiomarkerTableProps) => {
   const [biomarker, setBiomarker] = useState<BiomarkerModel | null>(null);
   const [results, setResults] = useState<number>(0);
-  const status: string = "Reviewed" 
+  const status: string = "Reviewed";
 
   useEffect(() => {
     const fetchData = async () => {
@@ -42,19 +44,109 @@ const BiomarkerTable = ({ biomarker_id }: BiomarkerTableProps) => {
   if (!biomarker) {
     return (
       <div className="loading-container">
-        <img src={loading} alt="Loading..."/>
+        <img src={loading} alt="Loading..." />
       </div>
-    )
+    );
   }
 
   return (
     <div className="results-container">
-        <h1 id="top-header">{biomarker.biomarker_id} Results</h1>
-        <div id="header-label">Results: {results}</div>
-      <Collapsible title='Test'>
-        <SingleField title="Biomarker ID" value={biomarker.biomarker_id}/>
-        <SingleField title="Status" value={status}/>
-        <BiomarkerComponentTable biomarker={biomarker} status={status}/>
+      <h1 id="top-header">{biomarker.biomarker_id} Results</h1>
+      <div id="header-label">Results: {results}</div>
+      <Collapsible title="Test">
+        <SingleField title="Biomarker ID" value={biomarker.biomarker_id} />
+        <SingleField title="Status" value={status} />
+        <BiomarkerComponentTable biomarker={biomarker} status={status} />
+        <div className="field underline">
+          <div className="field-title">Condition</div>
+        </div>
+        <div className="sub-component-container">
+          {biomarker.condition ? (
+            <div>
+              <SingleField
+                title="Condition ID"
+                value={biomarker.condition.recommended_name.condition_id}
+              />
+              <SingleField
+                title="Resource"
+                value={biomarker.condition.recommended_name.resource}
+              />
+              <SingleField
+                title="Recommended Name"
+                value={toTitleCase(biomarker.condition.recommended_name.name)}
+              />
+              <SingleField
+                title="Exact Synonyms"
+                value={
+                  biomarker.condition.synonyms &&
+                    biomarker.condition.synonyms.length > 0
+                    ? biomarker.condition.synonyms
+                      ?.map((s) => s.name)
+                      .join(", ")
+                    : "None"
+                }
+              />
+              <SingleField
+                title="Description"
+                value={biomarker.condition.recommended_name.description}
+              />
+              <SingleField
+                title="URL"
+                value={biomarker.condition.recommended_name.url}
+              />
+            </div>
+          ) : (
+            <div className="field-value">
+              No condition data.
+            </div>
+          )}
+        </div>
+        <div className="field underline">
+          <div className="field-title">Exposure Agent</div>
+        </div>
+        <div className="sub-component-container">
+          {biomarker.exposure_agent ? (
+            <div>
+              <SingleField
+                title="Exposure Agent ID"
+                value={biomarker.exposure_agent.recommended_name.exposure_agent_id}
+              />
+              <SingleField
+                title="Resource"
+                value={biomarker.exposure_agent.recommended_name.name}
+              />
+              <SingleField
+                title="Recommended Name"
+                value={toTitleCase(biomarker.exposure_agent.recommended_name.name)}
+              />
+              <SingleField
+                title="Description"
+                value={biomarker.exposure_agent.recommended_name.description}
+              />
+              <SingleField
+                title="URL"
+                value={biomarker.exposure_agent.recommended_name.url}
+              />
+            </div>
+          ) : (
+            <div className="field-value">
+              No Exposure Agent data.
+            </div>
+          )}
+        </div>
+        <div className="field-title underline">
+          Top Level Evidence Source(s)
+        </div>
+          {biomarker.evidence_source ? (
+            <EvidenceSourceTable evidences={biomarker.evidence_source}/>
+          ) : (
+            <div className="field-value">
+              No top level evidence. 
+            </div>
+          )}
+        <div className="field-title underline">
+          Citation Information
+        </div>
       </Collapsible>
     </div>
   );
