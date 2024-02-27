7b00c87d030bba01f4d6fb48118f21b308bc4271
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-02-27 12:35:10 -0500

    finished biomarker component rendering

--- src/components/BiomarkerComponentTable.tsx
+++ src/components/BiomarkerComponentTable.tsx
@@ -9,61 +9,83 @@ import Collapsible from "./Collapsible";
 type BiomarkerComponentTableProps = {
   biomarker: BiomarkerModel;
   status: string;
-}
+};
 
 // Component //
 
-const BiomarkerComponentTable = ({biomarker, status}: BiomarkerComponentTableProps) => {
+const BiomarkerComponentTable = ({
+  biomarker,
+  status,
+}: BiomarkerComponentTableProps) => {
   return (
     <div>
-      <h2 className="section-header">Biomarker Component(s)</h2>
-      {biomarker.biomarker_component.map((component, index) => 
+      <h2 className="section-header">Biomarker Component(s):</h2>
+     {biomarker.biomarker_component.map((component, index) => (
         <Collapsible title={`Component: ${index + 1}`} isNested={true}>
-          <SingleField title="Biomarker" value={component.biomarker}/>
+          <SingleField title="Biomarker" value={component.biomarker} />
           <div className="field underline">
             <div className="field-title">Assessed Biomarker Entity:</div>
           </div>
           <div className="sub-component-container">
-            <SingleField title="Recommended Name" value={component.assessed_biomarker_entity.recommended_name}/>
-            <SingleField title="Synonyms" value={
-                  component.assessed_biomarker_entity.synonyms && component.assessed_biomarker_entity.synonyms.length > 0
-                    ? component.assessed_biomarker_entity.synonyms?.map(s => s.synonym).join(', ')
-                    : 'None'
-                }/>
+            <SingleField
+              title="Recommended Name"
+              value={component.assessed_biomarker_entity.recommended_name}
+            />
+            <SingleField
+              title="Synonyms"
+              value={
+                component.assessed_biomarker_entity.synonyms &&
+                  component.assessed_biomarker_entity.synonyms.length > 0
+                  ? component.assessed_biomarker_entity.synonyms
+                    ?.map((s) => s.synonym)
+                    .join(", ")
+                  : "None"
+              }
+            />
           </div>
-          <SingleField title="Assessed Biomarker Entity ID" value={component.assessed_biomarker_entity_id}/>
-          <SingleField title="Assessed Entity Type" value={component.assessed_entity_type}/>
+          <SingleField
+            title="Assessed Biomarker Entity ID"
+            value={component.assessed_biomarker_entity_id}
+          />
+          <SingleField
+            title="Assessed Entity Type"
+            value={component.assessed_entity_type}
+          />
           <div className="field">
             <div className="field-title underline">Specimen(s):</div>
           </div>
           <div className="sub-component-container">
-            {
-              component.specimen && component.specimen.length > 0
-              ? component.specimen.map((specimen) => (
+            {component.specimen && component.specimen.length > 0 ? (
+              component.specimen.map((specimen) => (
                 <>
-                  {Object.entries(specimen).map(([key, value]) => (
-                    value && (
-                      <SingleField
-                          title={key.replace(/_/g, ' ')}
-                          value = {
-                            component.specimen?.map(s => s[key as keyof Specimen])
-                              .filter(v => v != null)
-                              .join(', ') || 'None'
+                  {Object.entries(specimen).map(
+                    ([key, value]) =>
+                      value && (
+                        <SingleField
+                          title={key.replace(/_/g, " ")}
+                          value={
+                            component.specimen
+                              ?.map((s) => s[key as keyof Specimen])
+                              .filter((v) => v != null)
+                              .join(", ") || "None"
                           }
-                      />
-                    )
-                  ))}
+                        />
+                      ),
+                  )}
                 </>
               ))
-              : <div className="field-value">None</div>
-            }
+            ) : (
+              <div className="field-value">None</div>
+            )}
           </div>
-          <div className="field-title underline">Component Evidence Source(s)</div>
-          <EvidenceSourceTable evidences={component.evidence_source}/>
+          <div className="field-title underline">
+            Component Evidence Source(s)
+          </div>
+          <EvidenceSourceTable evidences={component.evidence_source} />
         </Collapsible>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default BiomarkerComponentTable;
